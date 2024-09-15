07fa65843fc7426329c3b1041fb9184cedeb7811
Francesca Parris <mailbox787878@gmail.com>
2024-09-15 22:37:26 +0100

    initial_commit

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const images = document.querySelectorAll("img");
+
+    for(const image of images){
+        console.log(image)
+
+    }
+
+    })
